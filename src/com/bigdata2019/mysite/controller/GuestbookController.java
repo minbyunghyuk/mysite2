34eb9390443a3f505b8e35ce39124d74b16b4dd7
BIT <BIT@DESKTOP-S4KDBK1>
2019-12-23 10:47:52 +0900

    mysite #2

--- src/com/bigdata2019/mysite/controller/GuestbookController.java
+++ src/com/bigdata2019/mysite/controller/GuestbookController.java
@@ -1,7 +1,6 @@
 package com.bigdata2019.mysite.controller;
 
 import java.io.IOException;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -9,13 +8,13 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.bigdata2019.mysite.web.util.WebUtil;
 
-public class MainController extends HttpServlet {
+public class GuestbookController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
-		
-		WebUtil.forward(request, response, "/WEB-INF/views/main/index.jsp");
+
+		WebUtil.forward(request, response, "/WEB-INF/views/guestbook/list.jsp");
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
